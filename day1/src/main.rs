d2eb6b3b900baec89a8cbdd9a172c08bb1230578
cd <cd@cds-MacBook-Pro.local>
2024-12-03 10:42:53 +1300

    day1 doneN

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,7 @@
+use std::collections::HashMap;
 use std::error::Error;
-use std::io::Read;
 use std::fs::File;
+use std::io::Read;
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
@@ -10,12 +11,10 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let file_content = String::from_utf8(contents)?;
 
-    //split the file content into lines
     let mut first_half = Vec::new();
     let mut second_half = Vec::new();
 
     for line in file_content.lines() {
-        //split each line into parts by whitespace
         let mut parts = line.split_whitespace();
         if let (Some(first), Some(second)) = (parts.next(), parts.next()) {
             first_half.push(first.parse::<i32>()?);
@@ -23,7 +22,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    //print results
     println!("First half: {:?}", first_half);
     println!("Second half: {:?}", second_half);
     println!("first half len: {}", first_half.len());
@@ -31,12 +29,25 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     first_half.sort();
     second_half.sort();
+
+    let mut freq_map: HashMap<i32, i32> = HashMap::new();
+    for &num in &second_half {
+        *freq_map.entry(num).or_insert(0) += 1;
+    }
+
     let mut sum = 0;
-    let mut similarity = 0;
     for (a, b) in first_half.iter().zip(second_half.iter()) {
         sum +=(a-b).abs();
-        similarity += second_half.iter().filter(|&&x| x == *a).count() as i32*a;
     }
+
+
+    let mut similarity = 0;
+    for &a in &first_half {
+        if let Some(&count) = freq_map.get(&a) {
+            similarity += count * a;
+        }
+    }
+
     println!("total sum is {}", sum);
     println!("total similarity is {}", similarity);
     Ok(())
